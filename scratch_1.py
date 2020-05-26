529c29b5f6d0d437644409e9f635a7e5c9462e91
pratikjoshi1 <51682201+pratikjoshi1@users.noreply.github.com>
2020-05-26 01:09:52 -0700

    Add files via upload

--- scratch_1.py
+++ scratch_1.py
@@ -1,45 +1,47 @@
-import time
-from datetime import datetime
 import requests
+host_path="/etc/hosts"
 def block_website(website_block):
-
-    host_path="C:\Windows\System32\drivers\etc\hosts"
+    global host
     redirect="127.0.0.1"
     with open(host_path,'r+') as file:
         content=file.read()
         for website in website_block:
             if website in content:
+                print("It is already blocked")
                 pass
             else:
                 file.write(redirect+" "+website+"\n")
         print(content)
-
-
 def site_avaliable(x):
-   # response = requests.get("http://"+x)
     try :
         response = requests.get("http://" + x)
         if int(response.status_code)==200:
             website_block.append(site)
-
         else:
             print("Site is not reachable")
     except:
         print("Wrong value Try again")
 def unblock_website(website):
-    if website in website_block:
-        website_block.remove(website)
-    else:
-        print("Wrong value")
+    global host_path
 
+    with open(host_path,'r') as file:
+        content = file.readlines()
+        #if ("127.0.0.1 "+website) not in content:
+         #   print("This website is not blocked")
+    with open(host_path,'w') as file:
+        for line in content:
+            if line.strip("\n") != ("127.0.0.1 "+website):
+                file.write(line)
 website_block=[]
 y="y"
 while y=="y" or y=="yes":
     site=input("Enter website to Block: ")
     site_avaliable(site)
     y=input("Do you want block more website y or n? ")
-
-#unblock_website(input("Do you want unblock any website? If yes then enter otherwise skip"))
-
 print(website_block)
 block_website(website_block)
+while True:
+    z=(input("Do you want unblock any website? If yes then enter otherwise skip"))
+    unblock_website(z)
+    if z=='n':
+        break
